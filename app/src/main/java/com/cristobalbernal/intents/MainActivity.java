1f7f1896de7386669a5e85660f73e361d2b05937
Cristobal Bernal Mayordomo <90463533+Racriberny@users.noreply.github.com>
2022-12-19 18:27:53 +0100

    Camara

--- app/src/main/java/com/cristobalbernal/intents/MainActivity.java
+++ app/src/main/java/com/cristobalbernal/intents/MainActivity.java
@@ -1,20 +1,29 @@
 package com.cristobalbernal.intents;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> photoActivityResultLauncher;
     Button abrir;
     Button llamar;
     Button abrirPaginaWeb;
@@ -22,19 +31,35 @@ public class MainActivity extends AppCompatActivity {
     Button email;
     Button camara;
     private final int DEFAULT_ZOOM = 17;
+    private final int TAKE_PHOTO = 2;
+    private ImageView ivPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] to = {"joanmonvi@gmail.com "};
+        String[] to = {"joanmonvi@gmail.com,daminto2001@gmail.com "};
         abrir = findViewById(R.id.abrirAplicacion);
         llamar = findViewById(R.id.llamar);
         abrirPaginaWeb = findViewById(R.id.abrirPaginaWeb);
         mapa = findViewById(R.id.maps);
         email = findViewById(R.id.email);
         camara = findViewById(R.id.camara);
-
+        ivPhoto = findViewById(R.id.imageView);
+        photoActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            assert data != null;
+                            Bundle extras = data.getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            ivPhoto.setImageBitmap(imageBitmap);
+                        }
+                    }
+                });
 
         abrir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                enviarEmail(to,"AsuntoPrueba","Hola Joan!!!");
+                enviarEmail(to,"AsuntoPrueba","Eres un payaso!!!");
             }
         });
         camara.setOnClickListener(new View.OnClickListener() {
@@ -77,8 +102,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void abrirCamara() {
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
+        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            if (photoIntent.resolveActivity(getPackageManager()) != null) {
+                int permision = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+                if (permision != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, TAKE_PHOTO);
+                } else {
+                    //Intent intent = new Intent(this, SomeActivity.class);
+                    photoActivityResultLauncher.launch(photoIntent);
+                    //startActivityForResult(photoIntent, REQUEST_IMAGE_CAPTURE);
+                    /* En onActivityResult recibiremos el resultado */
+                }
+            }
+        }
+
     }
 
     @SuppressLint("IntentReset")
@@ -89,7 +127,10 @@ public class MainActivity extends AppCompatActivity {
         emailIntent.putExtra(Intent.EXTRA_EMAIL,para);
         emailIntent.putExtra(Intent.EXTRA_SUBJECT,asunto);
         emailIntent.putExtra(Intent.EXTRA_TEXT,contexto);
-        startActivity(Intent.createChooser(emailIntent,"Send Email!!!!"));
+
+        if (emailIntent.resolveActivity(getPackageManager()) !=null){
+            startActivity(emailIntent);
+        }
     }
 
 
